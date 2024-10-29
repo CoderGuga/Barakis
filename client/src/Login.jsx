ba89e216fe5e7fe0f7762754d00b840324569ad2
CoderGuga <gabrielius.rut@gmail.com>
2024-10-29 20:54:37 +0200

    test loginas

---
+++ client/src/Login.jsx
@@ -0,0 +1,64 @@
+{/*import React from "react";*/}
+import { Link } from "react-router-dom"
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+function Login() {
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/login', {name, password})
+        .then(result => {console.log(result)
+            if(result.data === "Success"){
+                navigate('/home')
+            }
+        })
+        .catch (err=> console.log(err)) 
+    }
+
+    return(
+        <div className="d-flex justify-content-center allign-items-center bg-secondary vh-100">
+            <div className="bg-white p-3 rounded w-25">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Name</strong>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Name"
+                        autoComplete="off"
+                        name="name"
+                        className="form-control rounded-0"
+                        onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Password</strong>
+                    </label>
+                    <input
+                        type="password"
+                        placeholder="Enter Password"
+                        autoComplete="off"
+                        name="password"
+                        className="form-control rounded-0"
+                        onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 rounded-0">
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
+
+    );
+}
+
+export default Login;
