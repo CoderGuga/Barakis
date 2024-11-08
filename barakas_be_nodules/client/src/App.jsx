83bcd2235fa0b16694ba0363751fdbc2b97f860b
Gabrielius Rutkauskas <gabrielius.rut@gmail.com>
2024-11-08 12:37:48 +0200

    Add files via upload
    1h 9min

---
+++ barakas_be_nodules/client/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/navbar.component";
+import { Route, Routes } from "react-router-dom";
+import UserAuthForm from "./pages/userAuthForm.page";
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navbar />}>
+                <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+                <Route path="signup" element={<UserAuthForm type="sing-up" />} />
+            </Route>
+        </Routes>
+    )
+}
+
+export default App;
