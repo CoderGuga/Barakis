b4d280a81fe700270fa0c3b1f72a0954f42300e9
CoderGuga <gabrielius.rut@gmail.com>
2024-12-01 19:33:05 +0200

    my bad

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5000", // Adjust for local dev
+        origin: process.env.CLIENT_URL || "http://localhost:5000", // Adjust for local dev
         credentials: true,
     })
 );
