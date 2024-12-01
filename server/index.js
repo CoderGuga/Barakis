0a4549c43cfec370e279cdb851aef96e46b987fb
CoderGuga <gabrielius.rut@gmail.com>
2024-12-01 18:16:13 +0200

    deployment v???

--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ const cors = require('cors')
 const TextRoutes = require('./routes/textRoutes')
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
+const path = require('path');
 
 dotenv.config();    
 
 const app = express()
 app.use(express.json())
+
+const __dirname = path.resolve();
 
 app.use(cors())
 
@@ -19,6 +22,15 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/tasks', TextRoutes)
 app.use('/users', userRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log('server is running')
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    });
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
 })
